10f44617927dbba15d9a759937bfdf625510403d
root <smsmelshamey90@gmail.com>
2024-04-16 13:09:13 -0700

    airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,9 +55,7 @@ class FileStorage:
     def delete(self, obj=None):
         """Delete a given object from __objects, if it exists."""
         try:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            if key in self.__objects:
-                del self.__objects[key]
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
         except (AttributeError, KeyError):
             pass
 
