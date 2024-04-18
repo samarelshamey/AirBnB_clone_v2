191680583a1695c3973c12c659f055ef2db763ec
root <smsmelshamey90@gmail.com>
2024-04-17 17:40:50 -0700

    airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,13 +22,13 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is not None:
-            if type(cls) == str:
+            if isinstance(cls, str):
                 cls = eval(cls)
             cls_dict = {}
-        for k, v in self.__objects.items():
-            if type(v) == cls:
-                cls_dict[k] = v
-            return cls_dict
+            for k, v in self.__objects.items():
+                if isinstance(v, cls):
+                    cls_dict[k] = v
+                return cls_dict
         return self.__objects
 
     def new(self, obj):
@@ -54,10 +54,9 @@ class FileStorage:
 
     def delete(self, obj=None):
         """Delete a given object from __objects, if it exists."""
-        try:
-            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
-        except (AttributeError, KeyError):
-            pass
+        if obj is not None:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects.pop(key, None)
 
     def close(self):
         """Call the reload method."""
