83646ebbec83b83a8bae234aa4324481bdc811b7
root <smsmelshamey90@gmail.com>
2024-05-17 06:46:24 -0700

    flask

--- models/state.py
+++ models/state.py
@@ -20,8 +20,7 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City",  backref="state", cascade="delete")
-    if getenv("HBNB_TYPE_STORAGE") != "db"
-    and not hasattr(BaseModel, 'cities'):
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             """Get a list of all related City objects."""
