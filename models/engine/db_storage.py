8e2102447a90aa5b02811925d6d2c0895b836d1a
root <smsmelshamey90@gmail.com>
2024-05-17 09:19:16 -0700

    flask

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -77,8 +77,8 @@ class DBStorage:
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.__session = Session
 
     def close(self):
         """Close the working SQLAlchemy session."""
-        self.__session.close()
+        self.__session.remove()
