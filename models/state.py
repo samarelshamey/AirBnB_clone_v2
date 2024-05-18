de36dbbae63b468cb6f7034f780661f3c8964317
root <smsmelshamey90@gmail.com>
2024-05-18 01:24:16 -0700

    flask

--- models/state.py
+++ models/state.py
@@ -5,7 +5,6 @@ from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import models
 
 
 class State(BaseModel, Base):
